384167194c213f4630c58ede062e85b07ea6a299
utkazu <hiroshimazeal@gmail.com>
2020-03-05 16:52:15 +0900

    [Update] ビューの雛形粗方完成。ビュー雛形の残りはtableタグの活用のみ

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'books' => 'books#index'
+  get 'books' => 'books#index', as: 'books'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
 
   post 'books' => 'books#create'
-  patch 'todolist/:id' => 'books#update', as: 'update_book'
-  delete 'todolist/:id' => 'books#destroy', as: 'destroy_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
 end
